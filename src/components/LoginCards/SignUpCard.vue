6e5671d8f493f981d0f825b8ea4f2b61e70971bb
Niek <frankenstein795@gmail.com>
2024-08-23 10:12:20 +0200

    Improve signup feedback

--- src/components/LoginCards/SignUpCard.vue
+++ src/components/LoginCards/SignUpCard.vue
@@ -25,16 +25,23 @@ const handlePasswordValidation = debounce(() => {
 	invalidPassword.value = password === confirmPassword;
 }, 500);
 
+const signUpPressed = ref(false);
+
 async function signUp() {
 	const { email, password, firstName, lastName } = model.value;
+
+	signUpPressed.value = true;
 
 	const err = await userSignUp(email, password, firstName, lastName);
 
 	if (err) {
+		signUpPressed.value = false;
 		return;
 	}
 
 	emit("step", "joinOrCreate");
+
+	signUpPressed.value = false;
 }
 </script>
 
@@ -54,40 +61,72 @@ async function signUp() {
 
 			<div class="flex flex-col gap-4">
 				<div class="grid grid-cols-2 gap-4">
-					<TextInput id="firstName" v-model="model.firstName" :label="$t('login.signUp.labels.firstName')" />
 					<TextInput
-						id="lastName" v-model="model.lastName" :label="$t('login.signUp.labels.lastName')"
+						id="firstName"
+						v-model="model.firstName"
+						:label="$t('login.signUp.labels.firstName')"
+						:disabled="signUpPressed"
+					/>
+					<TextInput
+						id="lastName"
+						v-model="model.lastName"
+						:label="$t('login.signUp.labels.lastName')"
 						class="text-right"
+						:disabled="signUpPressed"
 					/>
 				</div>
 
 				<TextInput
-					id="email" v-model="model.email" :label="$t('login.signUp.labels.email')" enable-group
+					id="email"
+					v-model="model.email"
+					:label="$t('login.signUp.labels.email')" enable-group
 					:invalid="!validateEmail(model.email)"
+					:disabled="signUpPressed"
 				/>
 
 				<TextInput
-					id="password" v-model="model.password" :label="$t('login.signUp.labels.password')"
-					input-class="w-full" password password-feedback
+					id="password" v-model="model.password"
+					:label="$t('login.signUp.labels.password')"
+					input-class="w-full"
+					password
+					password-feedback
+					:disabled="signUpPressed"
 					@update:model-value="handlePasswordValidation"
 				/>
 
 				<TextInput
 					id="confirmPassword" v-model="model.confirmPassword"
-					:label="$t('login.signUp.labels.confirmPassword')" input-class="w-full"
-					:invalid="!invalidPassword" password
+					:label="$t('login.signUp.labels.confirmPassword')"
+					input-class="w-full"
+					:invalid="!invalidPassword"
+					:disabled="signUpPressed"
+					password
 					@update:model-value="handlePasswordValidation"
 				/>
 
-				<pv-message v-if="!invalidPassword" icon="pi pi-times-circle" severity="error">
+				<pv-message
+					v-if="!invalidPassword"
+					icon="pi pi-times-circle"
+					severity="error"
+				>
 					{{ $t("login.signUp.messages.passwordsNotMatching") }}
 				</pv-message>
 
-				<pv-message v-if="incorrect" icon="pi pi-times-circle" severity="error">
+				<pv-message
+					v-if="incorrect"
+					icon="pi pi-times-circle"
+					severity="error"
+				>
 					{{ $t("login.signUp.messages.invalidLogin") }}
 				</pv-message>
 
-				<pv-button class="w-full" :label="$t('login.signUp.buttons.signUp')" @click="signUp" />
+				<pv-button
+					class="w-full"
+					:label="$t('login.signUp.buttons.signUp')"
+					:disabled="signUpPressed"
+					:loading="signUpPressed"
+					@click="signUp"
+				/>
 			</div>
 		</div>
 
